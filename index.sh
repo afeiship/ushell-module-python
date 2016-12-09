c8cb25f53dbd04ea8b6cc1dff9eabbe52167c0ec
feizheng <1290657123@qq.com>
2016-12-09 10:50:19 +0800

    intial

---
+++ index.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+#  brew install yarn
+# npm install -g yarn
+alias yarn-install='curl -o- -L https://yarnpkg.com/install.sh | bash';
+
+
+unset ROOT_PATH;
