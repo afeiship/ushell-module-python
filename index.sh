7034ea85decafd7161b99cfe5dc35e2bd47101fc
feizheng <1290657123@qq.com>
2016-12-09 12:46:44 +0800

    add y cmd

--- index.sh
+++ index.sh
@@ -6,6 +6,8 @@ export PATH="$PATH:`yarn global bin`";
 #  brew install yarn
 # npm install -g yarn
 alias yarn-install='curl -o- -L https://yarnpkg.com/install.sh | bash';
+alias yi='yarn install';
+alias y='yarn';
 
 
 unset ROOT_PATH;
