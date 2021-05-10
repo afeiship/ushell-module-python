e1b2fe56106a07b5e004e02e5ced8d32442cce97
afeiship <1290657123@qq.com>
2021-05-10 14:59:52 +0800

    fix: yarn not found

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-export PATH="$PATH:`yarn global bin`";
+# export PATH="$PATH:`yarn global bin`";
 #  brew install yarn
 # npm install -g yarn
 alias yarn-install='curl -o- -L https://yarnpkg.com/install.sh | bash';
