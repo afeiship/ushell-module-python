fdd5ed09c86531e08ee6e2d2d0412558779d1325
afeiship <1290657123@qq.com>
2022-01-25 10:31:47 +0800

    chore: add spy-sh

--- modules/scrapy.sh
+++ modules/scrapy.sh
@@ -4,6 +4,8 @@ alias spy="scrapy";
 # generate project/spider
 alias spy-gp="scrapy startproject"; 
 alias spy-gs="scrapy genspider";
+
 # start
 alias spy-c="scrapy crawl";
+alias spy-sh="scrapy shell";
 
