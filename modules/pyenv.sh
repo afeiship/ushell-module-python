d691b3c4a9de54b20b7cd22b5a96b62d099b6a65
afeiship <1290657123@qq.com>
2022-03-15 22:27:10 +0800

    fix: usm type for linx

--- modules/pyenv.sh
+++ modules/pyenv.sh
@@ -5,8 +5,7 @@ export PATH="$PYENV_ROOT/shims:$PATH";
 eval "$(pyenv init -)";
 eval "$(pyenv virtualenv-init -)";
 
-if [[ $USM_OS_TYPE == 'macos' ]]; then
-    # todo
-else
-    export PATH="$PYENV_ROOT/bin:$PATH";
+if [[ $USM_OS_TYPE != 'macos' ]]
+then
+  export PATH="$PYENV_ROOT/bin:$PATH";
 fi
