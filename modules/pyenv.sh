df624a30770012f06f4493311b40cd7ca07f0784
afeiship <1290657123@qq.com>
2022-03-15 22:18:16 +0800

    feat: add bin to centos

--- modules/pyenv.sh
+++ modules/pyenv.sh
@@ -4,3 +4,9 @@ export PYENV_ROOT="$HOME/.pyenv";
 export PATH="$PYENV_ROOT/shims:$PATH";
 eval "$(pyenv init -)";
 eval "$(pyenv virtualenv-init -)";
+
+if [[ $USM_OS_TYPE == 'macos' ]]; then
+    # todo
+else
+    export PATH="$PYENV_ROOT/bin:$PATH";
+fi
