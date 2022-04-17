b84d7fc0249d332123ce10bd7535bc2881f8c390
afeiship <1290657123@qq.com>
2022-04-17 19:38:12 +0800

    fix: update pip1

--- modules/pip.sh
+++ modules/pip.sh
@@ -4,3 +4,7 @@ alias pi="pip3 install -U"
 alias pinx="pip3 install jsw-nx -U"
 alias pi-r="pip install -r requirements.txt "
 alias pi-fz="pip3 freeze > requirements.txt"
+
+# 常用的 poetry/orator
+alias pp1="pip install poetry";
+alias pp2="pip install orator";
