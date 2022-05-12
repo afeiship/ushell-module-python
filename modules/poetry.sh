75f254ac4cf025c0fa1df4a921ffdc2584dac80f
afeiship <1290657123@qq.com>
2022-05-13 06:58:17 +0800

    Release 1.0.17

--- modules/poetry.sh
+++ modules/poetry.sh
@@ -5,6 +5,7 @@ alias poo="poetry config pypi-token.pypi $PYPI_TOKEN";
 alias po-b="poetry build";
 alias po-p="poetry publish";
 alias po-r="poetry run";
+alias po-u="poetry update";
 alias po-i="poetry install";
 alias po-r="poetry remove";
 alias po-vp="poetry version patch";
