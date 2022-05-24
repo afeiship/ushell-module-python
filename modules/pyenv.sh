d97ef3e5a48ad3f1795af928733db1e96e8f40ed
afeiship <1290657123@qq.com>
2022-05-24 08:53:56 +0800

    Release 1.0.19

--- modules/pyenv.sh
+++ modules/pyenv.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 export PYENV_ROOT="$HOME/.pyenv";
-export PATH="$PYENV_ROOT/shims:$PATH";
-
-# add to bin for centos + ubuntu
-if [[ $USM_OS_TYPE != 'macos' ]]
-then
-  export PATH="$PYENV_ROOT/bin:$PATH";
-fi
+export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH";
 
 eval "$(pyenv init -)";
 eval "$(pyenv virtualenv-init -)";
+
+# if [[ $USM_OS_TYPE != 'macos' ]]
+# then
+#   export PATH="$PYENV_ROOT/bin:$PATH";
+# fi
