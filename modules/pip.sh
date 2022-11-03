3062403439402e9b5bfb7baea9b0a56c27133447
afeiship <1290657123@qq.com>
2022-11-03 19:45:23 +0800

    feat: pi => pp for pip

--- modules/pip.sh
+++ modules/pip.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-alias pi="pip3 install -U"
-alias pinx="pip3 install jsw-nx -U"
-alias pi-r="pip install -r requirements.txt "
-alias pi-fz="pip3 freeze > requirements.txt"
-
-# 常用的 poetry/orator
-alias pp1="pip install poetry";
-alias pp2="pip install orator";
+alias pp-i="pip3 install -U"
+alias pp-ir="pip install -r requirements.txt "
+alias pp-ifz="pip3 freeze > requirements.txt"
