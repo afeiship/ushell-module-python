3ee1ecfc70990d1fcef0714b0ce8500f738e33f7
aric.zheng <1290657123@qq.com>
2025-09-06 20:24:45 +0800

    fix: wrap pyenv initialization in a command check

--- modules/pyenv.sh
+++ modules/pyenv.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+
 export PYENV_ROOT="$HOME/.pyenv";
 export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH";
 
-eval "$(pyenv init -)";
-eval "$(pyenv virtualenv-init -)";
+if command -v pyenv >/dev/null 2>&1; then
+	eval "$(pyenv init -)"
+	eval "$(pyenv virtualenv-init -)"
+fi
 
 # if [[ $USM_OS_TYPE != 'macos' ]]
 # then
